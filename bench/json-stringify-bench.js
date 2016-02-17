4feb70fad83ddb312131cfd4648d0bb413cf2cb5
Cédric Ronvel <cedric.ronvel@gmail.com>
2016-02-17 18:51:33 +0100

    ubench benchmark

--- bench/json-stringify-bench.js
+++ bench/json-stringify-bench.js
@@ -1,22 +1,14 @@
-#!/usr/bin/env node
 
 
 var json = require( '../lib/json.js' ) ;
 var sample = require( './sample1.json' ) ;
 
 
-var ops = 5000 ;
+bench( 'JSON stringify' , 'Native JSON.stringify()' , function() {
+	JSON.stringify( sample ) ;
+} ) ;
 
-while ( true )
-{
-	var start = Date.now() ;
-	for ( i = 0 ; i < ops ; i++ ) { JSON.stringify( sample ) ; }
-	var first = Date.now() - start ;
-	
-	start = Date.now();
-	for ( i = 0 ; i < ops ; i++ ) { json.stringify( sample ) ; }
-	var second = Date.now() - start ;
-	
-	console.log( "Native JSON.stringify() took %s" , first ) ;
-	console.log( "Home-made json.stringify() took %s (%s times slower)\n" , second , second / first ) ;
-}
+bench( 'JSON stringify' , 'tree.json.stringify()' , function() {
+	json.stringify( sample ) ;
+} ) ;
+
