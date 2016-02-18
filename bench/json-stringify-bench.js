f7bfd4a74cddf3be82309076511fcd5d1fb8b86b
Cédric Ronvel <cedric.ronvel@gmail.com>
2016-02-18 15:06:08 +0100

    Bechmark: ubench v0.2.x ; "use strict" everywhere

--- bench/json-stringify-bench.js
+++ bench/json-stringify-bench.js
@@ -4,11 +4,14 @@ var json = require( '../lib/json.js' ) ;
 var sample = require( './sample1.json' ) ;
 
 
-bench( 'JSON stringify' , 'Native JSON.stringify()' , function() {
-	JSON.stringify( sample ) ;
-} ) ;
+benchmark( 'JSON stringify' , function() {
+	
+	competitor( 'Native JSON.stringify()' , function() {
+		JSON.stringify( sample ) ;
+	} ) ;
 
-bench( 'JSON stringify' , 'tree.json.stringify()' , function() {
-	json.stringify( sample ) ;
+	competitor( 'tree.json.stringify()' , function() {
+		json.stringify( sample ) ;
+	} ) ;
 } ) ;
 
